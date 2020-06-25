90f821f14b9753141eeaf6455a33bc782ff5c4ea
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-06-25 23:29:02 +0100

    Data and functions from openintro

---
+++ data-raw/county_complete-dataprep.R
@@ -0,0 +1,8 @@
+# Load previous version of openintro package containing previous version
+# of county data frame. I took the last commit on 2020-06-23:
+# remotes::install_github("https://github.com/OpenIntroStat/openintro@d2bac268faf3ec566deb79baf106f274c6de38a2", force = TRUE)
+library(openintro)
+library(usethis)
+
+# Add to package
+use_data(county_complete, overwrite = TRUE)
