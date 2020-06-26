87583035adcd53be20f5fa79dc7d91daef412957
Mine Çetinkaya-Rundel <cetinkaya.mine@gmail.com>
2020-06-26 16:42:32 +0100

    Remove non-ASCII character

--- data-raw/county-dataprep.R
+++ data-raw/county-dataprep.R
@@ -1,8 +1,17 @@
-# Load previous version of openintro package containing previous version
-# of county data frame. I took the last commit on 2020-06-23:
+# Load previous version of openintro package containing previous version of data frame.
+# I took the last commit on 2020-06-23:
 # remotes::install_github("https://github.com/OpenIntroStat/openintro@d2bac268faf3ec566deb79baf106f274c6de38a2", force = TRUE)
 library(openintro)
 library(usethis)
+library(dplyr)
+
+county <- openintro::county
+
+county <- county %>%
+  mutate(
+    name = as.character(name),
+    name = if_else(name == "Hoonah–Angoon Census Area", "Hoonah Angoon Census Area", name)
+    )
 
 # Add to package
 use_data(county, overwrite = TRUE)
