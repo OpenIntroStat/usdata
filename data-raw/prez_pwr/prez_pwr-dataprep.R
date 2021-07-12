62a579e338faaa5d63698d2b306bbfcae590a9e6
npaterno <nicholas.paterno@gmail.com>
2021-07-12 14:30:35 -0700

    cleaned variable names

--- data-raw/prez_pwr/prez_pwr-dataprep.R
+++ data-raw/prez_pwr/prez_pwr-dataprep.R
@@ -8,11 +8,12 @@ raw_data <- read.csv(here::here("data-raw/prez_pwr/prez_pwr.csv"))
 
 # cleaning: r-------------------------------
 
-prez_pwr <- raw_data %>% 
+prez_pwr <- raw_data %>%
   pivot_longer(cols = c(yes, no),
                names_to = "has_pwr",
-               values_to = "percentage") %>% 
-  uncount(percentage)
+               values_to = "percentage") %>%
+  uncount(percentage) %>%
+  janitor::clean_names()
 
 # save --------------------------------------------------------------------------
 
