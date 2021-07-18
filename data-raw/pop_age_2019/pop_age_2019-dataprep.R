a23643d7209ce211956f28234f60a1bf6fea39cd
npaterno <nicholas.paterno@gmail.com>
2021-07-18 14:50:02 -0700

    State level population data based on age and gender

---
+++ data-raw/pop_age_2019/pop_age_2019-dataprep.R
@@ -0,0 +1,13 @@
+library(readr)
+
+# load data ---------------------------------------------------------------------
+raw_data <- read.csv(here::here("data-raw/pop_age_2019/pop_age_2019.csv"))
+
+# cleaning ----------------------------------------------------------------------
+
+# no cleaning required
+pop_age_2019<- raw_data
+
+# save --------------------------------------------------------------------------
+
+usethis::use_data(pop_age_2019, overwrite = TRUE)
